31ab704707a362b3679cad368dbbb2c167fc5f94
Brian Zindler <zindlerb@gmail.com>
2019-04-14 23:02:15 -0700

    build for gh pages

--- config/aliases.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-	Root: path.resolve(__dirname, '../src'),
-	Components: path.resolve(__dirname, '../src/components'),
-	Utilities: path.resolve(__dirname, '../src/utilities')
-}
--- src/components/ExternalLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './ExternalLink.css'
-
-const ExternalLink = ({ href, children }) => (
-	<a className="external-link" href={href} target="_blank">{children} <i className="fas fa-external-link-alt"/></a>
-)
-
-export default ExternalLink
